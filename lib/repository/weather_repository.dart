55759c7a9029ca1bd6a139eefcf47163ae2ec06d
macneko-ayu <macneko.ayu@gmail.com>
2024-05-17 11:14:10 +0900

    fix: debugPrintがリリースモードで出力されないようにした

--- lib/repository/weather_repository.dart
+++ lib/repository/weather_repository.dart
@@ -1,17 +1,18 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_training/data/weather_condition.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 class WeatherRepository {
   final _client = YumemiWeather();
 
-  @override
   WeatherCondition? fetchSimpleWeather() {
     final response = _client.fetchSimpleWeather();
     try {
       return WeatherCondition.from(response);
     } on Exception catch (e) {
-      debugPrint(e.toString());
+      if (kDebugMode) {
+        debugPrint(e.toString());
+      }
       return null;
     }
   }
