ad949e9b6c758b2f434b7660713417cfc871f11b
macneko-ayu <macneko.ayu@gmail.com>
2024-05-20 18:29:33 +0900

    feat: エラーダイアログを表示するメソッドを実装した

--- lib/presentation/screen/weather/weather_screen.dart
+++ lib/presentation/screen/weather/weather_screen.dart
@@ -78,6 +78,25 @@ class _WeatherScreenState extends State<WeatherScreen> {
       }
     }
   }
+
+  Future<void> _showErrorDialog(String message) async {
+    await showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('Error'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class _WeatherImage extends StatelessWidget {
