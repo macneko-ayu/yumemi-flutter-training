8d75828825037a2b5b27290fec09cfc1563f0fd1
macneko-ayu <macneko.ayu@gmail.com>
2024-05-21 13:37:40 +0900

    feat: JSON を返却する API を呼ぶメソッドを Repository に追加した

--- lib/repository/weather_repository.dart
+++ lib/repository/weather_repository.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter_training/data/app_exception.dart';
 import 'package:flutter_training/data/weather_condition.dart';
+import 'package:flutter_training/data/weather_info.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 class WeatherRepository {
@@ -18,6 +21,27 @@ class WeatherRepository {
     try {
       final response = _client.fetchThrowsWeather(area);
       return WeatherCondition.from(response);
+    } on YumemiWeatherError catch (e) {
+      switch (e) {
+        case YumemiWeatherError.invalidParameter:
+          throw const InvalidParameterException();
+        case YumemiWeatherError.unknown:
+          throw const UnknownException();
+      }
+    } on UndefinedWeatherException {
+      rethrow;
+    }
+  }
+
+  WeatherInfo fetchWeather(String area, DateTime date) {
+    final jsonString = '''
+      {
+          "area": "$area",
+          "date": "${date.toUtc().toIso8601String()}"
+      }''';
+    try {
+      final response = _client.fetchWeather(jsonString);
+      return WeatherInfo.fromJson(jsonDecode(response) as Map<String, dynamic>);
     } on YumemiWeatherError catch (e) {
       switch (e) {
         case YumemiWeatherError.invalidParameter:
