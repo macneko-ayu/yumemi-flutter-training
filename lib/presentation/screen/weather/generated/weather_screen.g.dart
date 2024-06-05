1bd8ba3219eac60b7da130ba263bcc0c1551d133
macneko-ayu <macneko.ayu@gmail.com>
2024-06-05 11:21:53 +0900

    feat: WeatherNotifierを実装した

---
+++ lib/presentation/screen/weather/generated/weather_screen.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, duplicate_ignore
+
+part of '../weather_screen.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$weatherNotifierHash() => r'88124fc7e34860351489b64d34c48b18d0bc883b';
+
+/// See also [WeatherNotifier].
+@ProviderFor(WeatherNotifier)
+final weatherNotifierProvider =
+    AutoDisposeNotifierProvider<WeatherNotifier, Weather?>.internal(
+  WeatherNotifier.new,
+  name: r'weatherNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$weatherNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$WeatherNotifier = AutoDisposeNotifier<Weather?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- lib/presentation/screen/weather/weather_screen.dart
+++ lib/presentation/screen/weather/weather_screen.dart
@@ -5,6 +5,21 @@ import 'package:flutter_training/data/weather.dart';
 import 'package:flutter_training/data/weather_condition.dart';
 import 'package:flutter_training/gen/assets.gen.dart';
 import 'package:flutter_training/repository/weather_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'generated/weather_screen.g.dart';
+
+@riverpod
+class WeatherNotifier extends _$WeatherNotifier {
+  @override
+  Weather? build() => null;
+
+  void fetchWeather(String area, DateTime date) {
+    final weather =
+        ref.read(weatherRepositoryProvider).fetchWeather(area, date);
+    state = weather;
+  }
+}
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
