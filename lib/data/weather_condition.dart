919713399508163c519b9c5f4613d3e893567957
macneko-ayu <macneko.ayu@gmail.com>
2024-06-05 18:46:52 +0900

    refactor: 利用していないWeatherConditionのfactoryメソッドを削除した

--- lib/data/weather_condition.dart
+++ lib/data/weather_condition.dart
@@ -1,14 +1,5 @@
-import 'package:flutter_training/data/app_exception.dart';
-
 enum WeatherCondition {
   sunny,
   cloudy,
   rainy;
-
-  factory WeatherCondition.from(String name) {
-    return values.singleWhere(
-      (element) => element.name == name,
-      orElse: () => throw const UndefinedWeatherException(),
-    );
-  }
 }
